3cb537d9f7057d0480e34ba3e136b375531efaca
J-ln <julianrolon7@gmail.com>
2022-07-03 18:07:15 -0300

    add-maximo

--- Python_1_ISPC.py
+++ Python_1_ISPC.py
@@ -1,12 +1,14 @@
 lista = []
 
+
 def main():
     pedir5()
     print()
 
+
 def pedir5():
-# Función pedir5 hecha por Aaron Ramiro Waldemar Cortez
-    
+    # Función pedir5 hecha por Aaron Ramiro Waldemar Cortez
+
     i = 1
     while i <= 5:
         # La función pide 5 veces un número y lo va almacenando con cada vuelta en la lista
@@ -16,6 +18,8 @@ def pedir5():
 
 # Función suma hecha por Priscila Kwiatkowski
 # La función recibe como parámetro la lista y devuelve la suma total de todos sus elementos.
+
+
 def suma():
     total = 0
     for number in lista:
@@ -30,10 +34,20 @@ def suma():
 def minimo():
     minimo = lista[0]
     for number in lista:
-        if number < minimo :
+        if number < minimo:
             minimo = number
     print("El valor minimo de la lista es ", minimo)
 
+# Función minimo hecha por Julián Rolón
+# La funcion recorre la lista el imprime en consola el valor maximo entre sus elementos
+
+
+def maximo():
+    maximo = lista[0]
+    for number in lista:
+        if number > maximo:
+            maximo = number
+    print("El valor maximo de la lista es ", maximo)
+
 
 main()
-
